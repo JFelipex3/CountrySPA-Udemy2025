d422c477d8c8700c743d7017d4c0361beed1b871
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-04-14 16:05:57 -0400

    Clase 108 Curso Angular 2025 - Layout Components

--- src/app/country/country.routes.ts
+++ src/app/country/country.routes.ts
@@ -1,8 +1,16 @@
 import { Routes } from "@angular/router";
 import { ByCapitalPageComponent } from "./pages/by-capital-page/by-capital-page.component";
+import { CountryLayoutComponent } from "./layouts/CountryLayout/CountryLayout.component";
 
 export const countryRoutes: Routes = [
-  {path: '', component: ByCapitalPageComponent}
+  {
+    path: '',
+    component: CountryLayoutComponent,
+    children: [
+      {path: 'by-capital', component: ByCapitalPageComponent},
+      {path: '**', redirectTo: 'by-capital'},
+    ]
+  },
 ];
 
 //Necesario para realizar exportación por defecto y no usar .then en app.routes.ts
