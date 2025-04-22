607b5d215d36b6a929a0cd0a6a8388f6eae9898a
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-04-22 15:41:13 -0400

    Inputs y Tablas

--- src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { ListComponent } from "../../components/list/list.component";
+import { SearchInputComponent } from "../../components/search-input/search-input.component";
 
 @Component({
   selector: 'app-by-capital-page',
-  imports: [],
+  imports: [ListComponent, SearchInputComponent],
   templateUrl: './by-capital-page.component.html',
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+
+  onSearch( value: string ) {
+    console.log(value);
+  }
+}
