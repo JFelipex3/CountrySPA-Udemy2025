98bdf67130678b2c30c3a7226db3761961381ca0
Jhonnatan Machuca <jmachuca@tuxpan.com>
2025-04-25 17:26:44 -0400

    implementación de dbounce y cache capital con map

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { RESTCountry } from '../interfaces/rest-countries.interface';
-import { catchError, delay, map, Observable, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import type { Country } from '../interfaces/country.interface';
 import { CountryMapper } from '../mappers/country.mapper';
 
@@ -13,14 +13,22 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
 
   private http = inject(HttpClient);
+  // Map para par valor v/s Set que almacena valores únicos
+  private queryCacheCapital = new Map<string, Country[]>();
 
   searchByCapital( query: string ): Observable<Country[]> {
     const lowerCaseQuery = query.toLowerCase();
+
+    if (this.queryCacheCapital.has(lowerCaseQuery)) {
+      return of(this.queryCacheCapital.get(lowerCaseQuery) as Country[] ?? []);
+    }
+
     const url = `${API_URL}/capital/${query}`;
 
     return this.http.get<RESTCountry[]>(url)
       .pipe(
         map( restCountries => CountryMapper.mapRestCountryArrayToCountryArray(restCountries) ),
+        tap( countries => this.queryCacheCapital.set(lowerCaseQuery, countries) ),
         catchError( error => {
           console.log('Error fetching ', error);
           return throwError( () => new Error(`No se pudo obtener países que coincidan con el valor ${query}`) );
@@ -35,7 +43,7 @@ export class CountryService {
     return this.http.get<RESTCountry[]>(url)
       .pipe(
         map( restCountries => CountryMapper.mapRestCountryArrayToCountryArray(restCountries) ),
-        delay( 3000 ),
+        //delay( 3000 ),
         catchError( error => {
           console.log('Error fetching ', error);
           return throwError( () => new Error(`No se pudo obtener países que coincidan con el valor ${query}`) );
